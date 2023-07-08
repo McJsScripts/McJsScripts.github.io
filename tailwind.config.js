0344a99ecd4b515bffdbb00323ec7eac3cfd353e
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2023-07-08 17:44:37 +0300

    SERVER

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./**/*.{html,js}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {
-			colors: {
-				primary: "#122849",
-				secondary: "#19355E",
-				tertiary: "#27497A",
-
-				background: "rgb(15 23 42)",
-				gray: "rgb(214 211 209)",
-			},
-		},
-		fontFamily: {
-			sans: "'Inter Variable', sans-serif",
-		},
+		extend: {}
 	},
 	plugins: [],
+	darkMode: false
 };
